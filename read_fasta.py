6897b8ad9752947e22b892eb0c96e74ae291d005
Harriet Dashnow <hdashnow@harriets-mbp.rch.org.au>
2014-03-25 14:31:08 +1100

    Deleted output fastas

--- read_fasta.py
+++ read_fasta.py
@@ -1,14 +1,17 @@
 from Bio import SeqIO
 
-infilename = "resistance_sample.fasta"
-outfilename = "resistance_sample.blaOXA.fasta"
+infilename = "resistance.fasta"
+outfilename = "resistance.blaOXA.fasta"
 
 blaOXA_records = []
 
+count = 0
 for seq_record in SeqIO.parse(infilename, "fasta"):
     #print seq_record.id
     #print seq_record.seq
     if "blaOXA" in seq_record.id:
         blaOXA_records.append(seq_record)
+        count += 1
 
 SeqIO.write(blaOXA_records, outfilename, "fasta")
+print count
